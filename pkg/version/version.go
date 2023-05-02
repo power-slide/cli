2fd545e539e8ed233691d4e2d2458a696bda65dd
Kaz Walker <me@kaz.codes>
2023-05-01 23:18:38 -0600

    chore: Add timeout to latest release API call.

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"runtime"
 	"strings"
+	"time"
 
 	"github.com/google/go-github/github"
 	log "github.com/sirupsen/logrus"
@@ -20,10 +21,18 @@ func IsReleaseBuild() bool {
 
 func LatestRelease() (*github.RepositoryRelease, error) {
 	client := github.NewClient(nil)
-	release, resp, err := client.Repositories.GetLatestRelease(context.TODO(), "power-slide", "cli")
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	release, resp, err := client.Repositories.GetLatestRelease(ctx, "power-slide", "cli")
 	if resp.StatusCode >= 400 {
 		return release, fmt.Errorf("release not found")
 	}
+
+	if ctx.Err() != nil {
+		err = ctx.Err()
+	}
+
 	return release, err
 }
 
